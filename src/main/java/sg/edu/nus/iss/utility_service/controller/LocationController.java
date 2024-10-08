181dfe28a76241d8e093356c614811e66bd88656
Anvisimi <8444.sa@gmail.com>
2024-10-08 11:39:36 +0800

    shopsmart-73

---
+++ src/main/java/sg/edu/nus/iss/utility_service/controller/LocationController.java
@@ -0,0 +1,26 @@
+package sg.edu.nus.iss.utility_service.controller;
+
+import com.google.maps.model.LatLng;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sg.edu.nus.iss.utility_service.service.LocationService;
+
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping("/coordinates")
+    public ResponseEntity<?> getCoordinates(@RequestParam String pincode) {
+        try {
+            LatLng coordinates = locationService.getCoordinatesFromPincode(pincode);
+            return new ResponseEntity<>(coordinates, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error Fetching Coordinates", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
