274940f744ac10074cb8e8b6d00150287729bec1
Anvisimi <8444.sa@gmail.com>
2024-10-08 10:27:59 +0800

    initial commit

---
+++ src/test/java/sg/edu/nus/iss/utility_service/UtilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.utility_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
