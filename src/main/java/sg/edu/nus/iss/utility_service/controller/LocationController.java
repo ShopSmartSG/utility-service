f96a556835503c7555f4541fbbee64ca4b5314a9
Anvisimi <8444.sa@gmail.com>
2024-10-10 23:39:43 +0800

    added swagger

--- src/main/java/sg/edu/nus/iss/utility_service/controller/LocationController.java
+++ src/main/java/sg/edu/nus/iss/utility_service/controller/LocationController.java
@@ -1,6 +1,8 @@
 package sg.edu.nus.iss.utility_service.controller;
 
 import com.google.maps.model.LatLng;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +11,14 @@ import sg.edu.nus.iss.utility_service.service.LocationService;
 
 @RestController
 @RequestMapping("/location")
+@Tag(name = "Location", description = "Get location coordinates via APIs")
 public class LocationController {
 
     @Autowired
     private LocationService locationService;
 
     @GetMapping("/coordinates")
+    @Operation(summary = "Get coordinates from pincode")
     public ResponseEntity<?> getCoordinates(@RequestParam String pincode) {
         try {
             LatLng coordinates = locationService.getCoordinatesFromPincode(pincode);
