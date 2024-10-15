94ffea0c4b184f070351d6725a8cdf85a315e210
Anvisimi <8444.sa@gmail.com>
2024-10-15 21:58:19 +0800

    working filebeat with some filebeat protocol issue but logs are working in logstash

--- src/main/java/sg/edu/nus/iss/utility_service/controller/LocationController.java
+++ src/main/java/sg/edu/nus/iss/utility_service/controller/LocationController.java
@@ -3,6 +3,8 @@ package sg.edu.nus.iss.utility_service.controller;
 import com.google.maps.model.LatLng;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +18,18 @@ public class LocationController {
 
     @Autowired
     private LocationService locationService;
+    Logger logger = LoggerFactory.getLogger(LocationController.class);
 
     @GetMapping("/coordinates")
     @Operation(summary = "Get coordinates from pincode")
     public ResponseEntity<?> getCoordinates(@RequestParam String pincode) {
+
         try {
+            logger.info("{\"message\": \"Fetching Coordinates for Pincode: " + pincode + "\"}");
             LatLng coordinates = locationService.getCoordinatesFromPincode(pincode);
             return new ResponseEntity<>(coordinates, HttpStatus.OK);
         } catch (Exception e) {
+            logger.info("{\"message\": \"Error Fetching Coordinates for Pincode: " + pincode + "\"}");
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
