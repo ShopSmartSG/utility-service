dba55d60485ffc90ce78f6cf28318e71066e9f62
Anvisimi <8444.sa@gmail.com>
2025-03-05 15:28:47 +0800

    added SAST and DAST checks

--- src/main/java/sg/edu/nus/iss/utility_service/UtilityServiceApplication.java
+++ src/main/java/sg/edu/nus/iss/utility_service/UtilityServiceApplication.java
@@ -2,8 +2,9 @@ package sg.edu.nus.iss.utility_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class UtilityServiceApplication {
 
 	public static void main(String[] args) {
